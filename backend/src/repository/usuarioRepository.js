f07165252a9d8a7466c8535c208eccef4f445bb9
1SnowFall1 <lms.mat2008@gmail.com>
2024-10-28 10:41:52 -0300

    crypto senha

--- backend/src/repository/usuarioRepository.js
+++ backend/src/repository/usuarioRepository.js
@@ -1,20 +1,22 @@
 import con from "./connection.js";
+import crypto from 'crypto-js';
 
 // Função para inserir um novo usuário
 export async function inserirUsuario(pessoa) {
     const comando = `
-        insert into tb_usuario (nome, idade, cpf, nm_celular, email, sexo, senha) 
+        insert into tb_usuario (nome, dt_nascimento, cpf, nm_celular, email, sexo, senha) 
         values (?, ?, ?, ?, ?, ?, ?)
     `;
     
+    let hash = crypto.SHA256(pessoa.senha).toString()
     let resposta = await con.query(comando, [
         pessoa.nome, 
-        pessoa.idade, 
+        pessoa.dt_nascimento, 
         pessoa.cpf, 
-        pessoa.nmCelular, 
+        pessoa.nm_celular, 
         pessoa.email, 
         pessoa.sexo, 
-        pessoa.senha
+        hash
     ]);
     let info = resposta[0];
     
@@ -33,7 +35,12 @@ export async function validarUsuario(pessoa) {
             and email = ?
             and senha = ?
     `;
-    
-    let registros = await con.query(comando, [pessoa.cpf, pessoa.email, pessoa.senha]);
-    return registros[0].length > 0 ? registros[0][0] : null; // Retorna null se não encontrar
+    let hash = crypto.SHA256(pessoa.senha).toString()
+    let registros = await con.query(comando, [pessoa.cpf, pessoa.email, hash]);
+    return registros[0].length  > 0 ? registros[0][0] : null; // Retorna null se não encontrar
 }
+
+
+
+
+
